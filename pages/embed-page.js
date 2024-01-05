8d4511f834d0b8844610b1bc184d80e9883674fb
Kyle Mo <kmo@netskope.com>
2024-01-05 13:19:38 +0800

    initial setup

---
+++ pages/embed-page.js
@@ -0,0 +1,13 @@
+import styles from '@/styles/Home.module.css'
+
+export default function Embed() {
+  return (
+    <>
+      <main className={`${styles.main}`}>
+        <div>
+            <a href="https://example.com" id="embedLink">Click Me</a>
+        </div>
+      </main>
+    </>
+  )
+}
